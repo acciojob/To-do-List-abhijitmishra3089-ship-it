5625e2baa96139c666a67ca3c1f97ab37b37b9d5
Abhijeet Mishra <abhijitmishra3089@gmail.com>
2026-05-28 21:56:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const btn=document.getElementById("addTodoBtn");
 const list=document.getElementById("todoList");
 btn.addEventListener("click",()=>{
 	let task=input.value
+	if(task === "") return
 	const li=document.createElement("li");
 	li.innerText=task
 	list.appendChild(li)
